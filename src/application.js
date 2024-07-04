57ffd1cda7284b41448f3e36994bd7ea5a9e2a20
Didier Djakoua <didier.djakoua@gmail.com>
2024-07-04 19:23:24 +0200

    first commit

---
+++ src/application.js
@@ -0,0 +1,17 @@
+import puppeteer from "puppeteer";
+
+class Application {
+    static async main() {
+        const browser = await puppeteer.launch({headless: 'shell'});
+        const page = await browser.newPage();  
+        await page.setContent('<html><body><h1>Test</h1></body></html>', { waitUntil: 'domcontentloaded' });
+        const pdfBuffer = await page.pdf({
+            format: 'A4',
+            path: './test.pdf',
+            printBackground: true
+        });
+        await browser.close()
+    }
+}
+
+export default Application;
